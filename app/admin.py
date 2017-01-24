a68379e6c0d893c7b20e9ceaa9a672c4282d33a1
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2017-01-24 16:58:10 -0300

    inser templates

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Livro
+
+
+class LivroAdmin(admin.ModelAdmin):
+    """
+    Admin class for blog posts.
+    """
+    list_display = (
+        'titulo', 'dono', 'isbn', 'autor', 'imagem', 'arquivo', 'tipo_livro', 'criado_em',
+        'editado_em', 'status')
+    list_filter = ('tipo_livro', 'dono', 'status')
+
+
+admin.site.register(Livro, LivroAdmin)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,44 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Auditavel(models.Model):
+    class Meta:
+        abstract = True
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    editado_em = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+
+# class Genero(Auditavel):
+#     nome = models.CharField(max_length=100)
+#
+#     class Meta:
+#         verbose_name = "Genero"
+#         verbose_name_plural = "Generos"
+#         ordering = ("nome",)
+
+
+TIPO = (
+    ('FISICO', 'FISICO'),
+    ('EBOOK', 'EBOOK')
+)
+
+
+class Livro(Auditavel):
+    titulo = models.CharField(max_length=300)
+    dono = models.ForeignKey(User, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=100, blank=True, null=True, verbose_name='ISBN')
+    autor = models.CharField(max_length=100, blank=True, null=True)
+    imagem = models.URLField(blank=True, null=True)
+    arquivo = models.URLField()
+    tipo_livro = models.CharField(max_length=100, choices=TIPO, verbose_name='Tipo do Livro')
+
+    class Meta:
+        verbose_name = "Livro"
+        verbose_name_plural = "Livros"
+        ordering = ("titulo",)
